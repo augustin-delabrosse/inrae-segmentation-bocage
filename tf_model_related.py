0af4923059ad443645b0848e4c449718787f2caf
augustin-delabrosse <augustin.de-la-brosse@hec.edu>
2023-11-23 18:09:00 +0100

    trying more regularization

--- tf_model_related.py
+++ tf_model_related.py
@@ -1,10 +1,12 @@
 import tensorflow as tf 
 from tensorflow import keras
+from keras.regularizers import l2
 from keras.models import Model
 from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Conv2DTranspose, AveragePooling2D
 from keras.layers import Activation, add, multiply, Lambda
 from keras.layers import UpSampling2D, Dropout, BatchNormalization
 from keras.initializers import glorot_normal
+from keras.layers import Layer
 
 from keras import backend as K
 
@@ -13,6 +15,40 @@ warnings.simplefilter("ignore")
 
 K.set_image_data_format('channels_last')  # Set the data format to 'channels_last' (TensorFlow dimension ordering)
 
+
+
+# inspired from https://datascience.stackexchange.com/questions/58884/how-to-create-custom-activation-functions-in-keras-tensorflow#comment119171_66358
+def tv_sigmoid(x):
+    return K.sigmoid(x)
+    
+class TVSigmoid(Layer):
+    def __init__(self, mu=1.0, trainable=False, **kwargs):
+        super(TVSigmoid, self).__init__(**kwargs)
+        self.supports_masking = True
+        self.mu = mu
+        self.trainable = trainable
+
+    def build(self, input_shape):
+        self.mu_factor = K.variable(self.mu,
+                                      dtype=K.floatx(),
+                                      name='mu_factor')
+        if self.trainable:
+            self._trainable_weights.append(self.mu_factor)
+
+        super(TVSigmoid, self).build(input_shape)
+
+    def call(self, inputs):
+        return tv_sigmoid(inputs)
+
+    def get_config(self):
+        config = {'mu': self.get_weights()[0] if self.trainable else self.mu,
+                  'trainable': self.trainable}
+        base_config = super(TVSigmoid, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+    
 
 # https://github.com/cpuimage/MaxDropout/tree/master
 class MaxDropout(tf.keras.layers.Layer):
@@ -261,7 +297,9 @@ class AttentionUnet:
 
         # Final upsampling and output
         up4 = concatenate([Conv2DTranspose(32, (3,3), strides=(2,2), padding='same', activation='relu', kernel_initializer=self.kinit)(up3), conv1], name='up4')
-        out = Conv2D(1, (1, 1), activation='sigmoid',  kernel_initializer=self.kinit, name='final')(up4)
+        out = Conv2D(1, (1, 1), kernel_initializer=self.kinit, name='final')(up4)
+        # out = Conv2D(1, (1, 1), activation='sigmoid',  kernel_initializer=self.kinit, name='final')(up4)
+        out = TVSigmoid(mu=1.0, trainable=True)(out)
 
         # Define the model with inputs and outputs
         model = Model(inputs=[inputs], outputs=[out])
@@ -373,3 +411,126 @@ class Losses:
         combo = (ce_d_w * weighted_ce) - ((1 - ce_d_w) * d)
         return combo
 
+
+
+class RegularizedAttentionUnet(AttentionUnet):
+    def __init__(self, input_size=(256, 256, 3), dropout_rate=0.2, l2_reg=1e-4):
+        self.dropout_rate = dropout_rate
+        self.l2_reg = l2_reg
+        self.input_size = input_size
+        # Define weight initialization method
+        self.kinit = 'glorot_normal'
+
+    def expend_as(self, tensor, rep, name):
+        my_repeat = Lambda(lambda x, repnum: K.repeat_elements(x, repnum, axis=3), arguments={'repnum': rep}, name='psi_up'+name)(tensor)
+        return my_repeat
+
+    def AttnGatingBlock(self, x, g, inter_shape, name):
+        # Get shapes of 'x' and 'g'
+        shape_x = K.int_shape(x)  # 32
+        shape_g = K.int_shape(g)  # 16
+
+        # Convolution on 'x' to match dimensions
+        theta_x = Conv2D(inter_shape, (2, 2), strides=(2, 2), padding='same', name='xl'+name)(x)  # 16
+        # Get shape of the convolved 'theta_x'
+        shape_theta_x = K.int_shape(theta_x)
+
+        # Convolution on 'g' to match dimensions
+        phi_g = Conv2D(inter_shape, (1, 1), padding='same')(g)
+        # Upsample 'g' to match 'theta_x' dimensions
+        upsample_g = Conv2DTranspose(inter_shape, (3, 3),strides=(shape_theta_x[1] // shape_g[1], shape_theta_x[2] // shape_g[2]),padding='same', name='g_up'+name)(phi_g)  # 16
+
+        # Concatenate upsampled 'g' and 'theta_x'
+        concat_xg = add([upsample_g, theta_x])
+
+        # Apply ReLU activation
+        act_xg = Activation('relu')(concat_xg)
+        # Calculate attention coefficients ('psi')
+        psi = Conv2D(1, (1, 1), padding='same', name='psi'+name)(act_xg)
+        # Apply sigmoid activation to obtain attention coefficients
+        sigmoid_xg = Activation('sigmoid')(psi)
+        # Get shape of sigmoid activation
+        shape_sigmoid = K.int_shape(sigmoid_xg)
+         # Upsample attention coefficients to match original spatial dimensions of 'x'
+        upsample_psi = UpSampling2D(size=(shape_x[1] // shape_sigmoid[1], shape_x[2] // shape_sigmoid[2]))(sigmoid_xg)  # 32
+
+        # Expand attention coefficients to match the number of channels in 'x'
+        upsample_psi = self.expend_as(upsample_psi, shape_x[3],  name) # Lambda(lambda x, repnum: K.repeat_elements(x, repnum, axis=3), arguments={'repnum': shape_x[3]}, name='psi_up'+name)(upsample_psi)# 
+        # Multiply attention coefficients and 'x' to obtain attended feature map
+        y = multiply([upsample_psi, x], name='q_attn'+name)
+
+        # Convolution on the attended feature map
+        result = Conv2D(shape_x[3], (1, 1), padding='same',name='q_attn_conv'+name)(y)
+        # Batch normalization on the result
+        result_bn = BatchNormalization(name='q_attn_bn'+name)(result)
+        return result_bn
+
+    def UnetConv2D(self, input, outdim, is_batchnorm, name):
+        x = Conv2D(outdim, (3, 3), strides=(1, 1), kernel_initializer=self.kinit, padding="same", 
+                   kernel_regularizer=l2(self.l2_reg), name=name+'_1')(input)
+        if is_batchnorm:
+            x = BatchNormalization(name=name + '_1_bn')(x)
+        x = Activation('relu', name=name + '_1_act')(x)
+        x = Dropout(self.dropout_rate, name='drop_' + name + '_1')(x)
+
+        x = Conv2D(outdim, (3, 3), strides=(1, 1), kernel_initializer=self.kinit, padding="same",
+                   kernel_regularizer=l2(self.l2_reg), name=name+'_2')(x)
+        if is_batchnorm:
+            x = BatchNormalization(name=name + '_2_bn')(x)
+        x = Activation('relu', name=name + '_2_act')(x)
+        x = Dropout(self.dropout_rate, name='drop_' + name + '_2')(x)
+        return x
+    
+    def UnetGatingSignal(self, input, is_batchnorm, name):
+        # Get the shape of the input tensor
+        shape = K.int_shape(input)
+        # 1x1 Convolution:
+        x = Conv2D(shape[3] * 1, (1, 1), strides=(1, 1), padding="same",  kernel_initializer=self.kinit, name=name + '_conv')(input)
+        if is_batchnorm:
+            # Batch Normalization (optional):
+            x = BatchNormalization(name=name + '_bn')(x)
+        # ReLU Activation:
+        x = Activation('relu', name = name + '_act')(x)
+        return x
+
+    def build_attention_unet(self):
+        # Input layer
+        inputs = Input(shape=self.input_size)
+
+        # Encoder part
+        conv1 = self.UnetConv2D(inputs, 32, is_batchnorm=True, name='conv1')
+        pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+        conv2 = self.UnetConv2D(pool1, 32, is_batchnorm=True, name='conv2')
+        pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+        conv3 = self.UnetConv2D(pool2, 64, is_batchnorm=True, name='conv3')
+        pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+        conv4 = self.UnetConv2D(pool3, 64, is_batchnorm=True, name='conv4')
+        pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+        # Central part
+        center = self.UnetConv2D(pool4, 128, is_batchnorm=True, name='center')
+
+        # Gating signals
+        g1 = self.UnetGatingSignal(center, is_batchnorm=True, name='g1')
+        attn1 = self.AttnGatingBlock(conv4, g1, 128, '_1')
+        up1 = concatenate([Conv2DTranspose(32, (3,3), strides=(2,2), padding='same', activation='relu', kernel_initializer=self.kinit)(center), attn1], name='up1')
+
+        g2 = self.UnetGatingSignal(up1, is_batchnorm=True, name='g2')
+        attn2 = self.AttnGatingBlock(conv3, g2, 64, '_2')
+        up2 = concatenate([Conv2DTranspose(64, (3,3), strides=(2,2), padding='same', activation='relu', kernel_initializer=self.kinit)(up1), attn2], name='up2')
+
+        g3 = self.UnetGatingSignal(up1, is_batchnorm=True, name='g3')
+        attn3 = self.AttnGatingBlock(conv2, g3, 32, '_3')
+        up3 = concatenate([Conv2DTranspose(32, (3,3), strides=(2,2), padding='same', activation='relu', kernel_initializer=self.kinit)(up2), attn3], name='up3')
+
+        # Final upsampling and output
+        up4 = concatenate([Conv2DTranspose(32, (3,3), strides=(2,2), padding='same', activation='relu', kernel_initializer=self.kinit)(up3), conv1], name='up4')
+        out = Conv2D(1, (1, 1), activation='sigmoid', kernel_initializer=self.kinit, name='final')(up4)
+
+        # Define the model with inputs and outputs
+        model = Model(inputs=[inputs], outputs=[out])
+
+        return model
