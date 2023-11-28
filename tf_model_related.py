f25b2b98cdc6f442cc9a12c9d3fd8d97cec335a5
augustin-delabrosse <augustin.de-la-brosse@hec.edu>
2023-11-28 11:46:51 +0100

    More regularization tries

--- tf_model_related.py
+++ tf_model_related.py
@@ -493,6 +493,49 @@ class RegularizedAttentionUnet(AttentionUnet):
         x = Activation('relu', name = name + '_act')(x)
         return x
 
+#     def build_attention_unet(self):
+#         # Input layer
+#         inputs = Input(shape=self.input_size)
+
+#         # Encoder part
+#         conv1 = self.UnetConv2D(inputs, 32, is_batchnorm=True, name='conv1')
+#         pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+#         conv2 = self.UnetConv2D(pool1, 32, is_batchnorm=True, name='conv2')
+#         pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+#         conv3 = self.UnetConv2D(pool2, 64, is_batchnorm=True, name='conv3')
+#         pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+#         conv4 = self.UnetConv2D(pool3, 64, is_batchnorm=True, name='conv4')
+#         pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+#         # Central part
+#         center = self.UnetConv2D(pool4, 128, is_batchnorm=True, name='center')
+
+#         # Gating signals
+#         g1 = self.UnetGatingSignal(center, is_batchnorm=True, name='g1')
+#         attn1 = self.AttnGatingBlock(conv4, g1, 128, '_1')
+#         up1 = concatenate([Conv2DTranspose(32, (3,3), strides=(2,2), padding='same', activation='relu', kernel_initializer=self.kinit)(center), attn1], name='up1')
+
+#         g2 = self.UnetGatingSignal(up1, is_batchnorm=True, name='g2')
+#         attn2 = self.AttnGatingBlock(conv3, g2, 64, '_2')
+#         up2 = concatenate([Conv2DTranspose(64, (3,3), strides=(2,2), padding='same', activation='relu', kernel_initializer=self.kinit)(up1), attn2], name='up2')
+
+#         g3 = self.UnetGatingSignal(up1, is_batchnorm=True, name='g3')
+#         attn3 = self.AttnGatingBlock(conv2, g3, 32, '_3')
+#         up3 = concatenate([Conv2DTranspose(32, (3,3), strides=(2,2), padding='same', activation='relu', kernel_initializer=self.kinit)(up2), attn3], name='up3')
+
+#         # Final upsampling and output
+#         up4 = concatenate([Conv2DTranspose(32, (3,3), strides=(2,2), padding='same', activation='relu', kernel_initializer=self.kinit)(up3), conv1], name='up4')
+#         out = Conv2D(1, (1, 1), activation='sigmoid', kernel_initializer=self.kinit, name='final')(up4)
+
+#         # Define the model with inputs and outputs
+#         model = Model(inputs=[inputs], outputs=[out])
+
+#         return model
+    
+    
     def build_attention_unet(self):
         # Input layer
         inputs = Input(shape=self.input_size)
@@ -507,22 +550,22 @@ class RegularizedAttentionUnet(AttentionUnet):
         conv3 = self.UnetConv2D(pool2, 64, is_batchnorm=True, name='conv3')
         pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-        conv4 = self.UnetConv2D(pool3, 64, is_batchnorm=True, name='conv4')
-        pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+        # conv4 = self.UnetConv2D(pool3, 64, is_batchnorm=True, name='conv4')
+        # pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
         # Central part
-        center = self.UnetConv2D(pool4, 128, is_batchnorm=True, name='center')
+        center = self.UnetConv2D(pool3, 128, is_batchnorm=True, name='center')
 
         # Gating signals
-        g1 = self.UnetGatingSignal(center, is_batchnorm=True, name='g1')
-        attn1 = self.AttnGatingBlock(conv4, g1, 128, '_1')
-        up1 = concatenate([Conv2DTranspose(32, (3,3), strides=(2,2), padding='same', activation='relu', kernel_initializer=self.kinit)(center), attn1], name='up1')
+        # g1 = self.UnetGatingSignal(center, is_batchnorm=True, name='g1')
+        # attn1 = self.AttnGatingBlock(conv4, g1, 128, '_1')
+        # up1 = concatenate([Conv2DTranspose(32, (3,3), strides=(2,2), padding='same', activation='relu', kernel_initializer=self.kinit)(center), attn1], name='up1')
 
-        g2 = self.UnetGatingSignal(up1, is_batchnorm=True, name='g2')
+        g2 = self.UnetGatingSignal(center, is_batchnorm=True, name='g2')
         attn2 = self.AttnGatingBlock(conv3, g2, 64, '_2')
-        up2 = concatenate([Conv2DTranspose(64, (3,3), strides=(2,2), padding='same', activation='relu', kernel_initializer=self.kinit)(up1), attn2], name='up2')
+        up2 = concatenate([Conv2DTranspose(64, (3,3), strides=(2,2), padding='same', activation='relu', kernel_initializer=self.kinit)(center), attn2], name='up2')
 
-        g3 = self.UnetGatingSignal(up1, is_batchnorm=True, name='g3')
+        g3 = self.UnetGatingSignal(up2, is_batchnorm=True, name='g3')
         attn3 = self.AttnGatingBlock(conv2, g3, 32, '_3')
         up3 = concatenate([Conv2DTranspose(32, (3,3), strides=(2,2), padding='same', activation='relu', kernel_initializer=self.kinit)(up2), attn3], name='up3')
 
